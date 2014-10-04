c14c9d876f9089ca68ff2cd885ce7de56887cff3
Dino Pejakovic <dino@voxdiversa.hr>
2014-10-04 11:29:57 +0200

    Fixed a comment.

--- powerline-installer.sh
+++ powerline-installer.sh
@@ -14,18 +14,18 @@ sudo fc-cache -vf
 sudo mv 10-powerline-symbols.conf /etc/fonts/conf.d/
 
 
-# Bind powerline to vim and set molokai theme, syntax, 256-color mode and line numbers
+# Bind powerline to vim, set 256-color mode.
 echo -e "
 set rtp+=/usr/local/lib/python2.7/dist-packages/powerline/bindings/vim/
 set laststatus=2
 set t_Co=256
 " >> /etc/vim/vimrc
 
-# Bind powerline to bash
+# Bind powerline to bash.
 echo -e "
 if [ -f /usr/local/lib/python2.7/dist-packages/powerline/bindings/bash/powerline.sh ]; then
     source /usr/local/lib/python2.7/dist-packages/powerline/bindings/bash/powerline.sh
 fi" >> /etc/bash.bashrc
 
-# Remove directory depth limit
+# Remove directory depth limit in powerline shell.
 sudo sed -i 's/\("dir_limit_depth":\).*/\1 null/' /usr/local/lib/python2.7/dist-packages/powerline/config_files/themes/shell/__main__.json
